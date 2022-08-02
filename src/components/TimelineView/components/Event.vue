06b1f18673f6041bc0e1978c4936c7d9b7094e51
TJ Fogarty <fogarty.tj@gmail.com>
2022-08-02 20:58:35 +0100

    Allows user to drag events around

--- src/components/TimelineView/components/Event.vue
+++ src/components/TimelineView/components/Event.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="event" :style="positionStyles" ref="target" draggable="true" @dragstart="handleDragStart"
-    @dragend="handleDragEnd">
+  <div tabindex="-1" class="event" :style="positionStyles" ref="target">
     <slot v-if="targetIsVisible" name="event" v-bind="{ item: data }">{{ data.name }}</slot>
   </div>
 </template>
 
 <script setup>
+// TODO: Drag and drop events
 import { ref, computed } from 'vue';
-import { useIntersectionObserver } from '@vueuse/core';
+import { useIntersectionObserver, useDraggable } from '@vueuse/core';
 import { useCurrentTimeline } from '../composables/useTimeline';
 
 const props = defineProps({
@@ -18,15 +18,21 @@ const props = defineProps({
 });
 
 const target = ref(null);
+const isDragging = ref(false);
+const draggingPos = ref({ x: 0, y: 0 });
 const targetIsVisible = ref(false);
-const { eventPositions, resourceHeight } = useCurrentTimeline();
+const { eventPositions, resourceHeight, hoveredResourceId, hoveredDate, datePositions, resourcePositions } = useCurrentTimeline();
 const resourceHeightPx = computed(() => {
   return `${resourceHeight}px`;
 });
 const position = computed(() => {
   if (!eventPositions.value[props.data.id]) return '';
+
   const pos = eventPositions.value[props.data.id];
-  return { x: pos.left, y: pos.top, w: pos.width };
+  const x = isDragging.value ? draggingPos.value.x : pos.left;
+  const y = isDragging.value ? draggingPos.value.y : pos.top;
+
+  return { x, y, w: pos.width };
 });
 const positionStyles = computed(() => {
   if (!position.value) return '';
@@ -38,14 +44,24 @@ useIntersectionObserver(target, ([{ isIntersecting }]) => {
   targetIsVisible.value = isIntersecting;
 });
 
-function handleDragStart(e) {
-  console.log('drag start', { e })
-  e.dataTransfer.dropEffect = 'move';
-}
-
-function handleDragEnd(e) {
-  console.log('drag end', { e });
-}
+useDraggable(target, {
+  onStart: () => {
+    isDragging.value = true;
+    const x = datePositions.value[hoveredDate.value.toFormat('y-MM-dd')];
+    const y = resourcePositions.value[hoveredResourceId.value];
+    draggingPos.value.x = x;
+    draggingPos.value.y = y;
+  },
+  onEnd: () => {
+    isDragging.value = false;
+  },
+  onMove: () => {
+    const x = datePositions.value[hoveredDate.value.toFormat('y-MM-dd')];
+    const y = resourcePositions.value[hoveredResourceId.value];
+    draggingPos.value.x = x;
+    draggingPos.value.y = y;
+  }
+});
 </script>
 
 <style scoped>
@@ -55,5 +71,6 @@ function handleDragEnd(e) {
   height: v-bind(resourceHeightPx);
   transform: var(--transform);
   width: var(--width);
+  transition: transform ease 0.1s;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App);
+
 app.config.performance = true;
+
 app.mount('#app');
