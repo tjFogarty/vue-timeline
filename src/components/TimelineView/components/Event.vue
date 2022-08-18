e7727154810bfef617d9212f8673268c45acadd0
TJ Fogarty <fogarty.tj@gmail.com>
2022-08-18 13:36:44 +0100

    Fixes drag position

--- src/components/TimelineView/components/Event.vue
+++ src/components/TimelineView/components/Event.vue
@@ -1,13 +1,12 @@
 <template>
-  <div tabindex="-1" class="event" :style="positionStyles" ref="target">
+  <div tabindex="-1" class="event" :style="positionStyles" ref="target" @mousedown.prevent="handleStartDrag" @mouseup="handleStopDrag">
     <slot v-if="targetIsVisible" name="event" v-bind="{ item: data }">{{ data.name }}</slot>
   </div>
 </template>
 
 <script setup>
-// TODO: Drag and drop events
 import { ref, computed } from 'vue';
-import { useIntersectionObserver, useDraggable } from '@vueuse/core';
+import { useIntersectionObserver } from '@vueuse/core';
 import { useCurrentTimeline } from '../composables/useTimeline';
 
 const props = defineProps({
@@ -19,49 +18,49 @@ const props = defineProps({
 
 const target = ref(null);
 const isDragging = ref(false);
-const draggingPos = ref({ x: 0, y: 0 });
 const targetIsVisible = ref(false);
-const { eventPositions, resourceHeight, hoveredResourceId, hoveredDate, datePositions, resourcePositions } = useCurrentTimeline();
+const { eventPositions, resourceHeight, hoveredResourceId, hoveredDate, datePositions, resourcePositions, columnWidth } = useCurrentTimeline();
+const dragOffset = ref(0);
 const resourceHeightPx = computed(() => {
   return `${resourceHeight}px`;
+});
+const draggingPos = computed(() => {
+  if (!isDragging.value) return null;
+
+  return {
+    x: datePositions.value[hoveredDate.value.toFormat('y-MM-dd')],
+    y: resourcePositions.value[hoveredResourceId.value],
+  }
 });
 const position = computed(() => {
   if (!eventPositions.value[props.data.id]) return '';
 
   const pos = eventPositions.value[props.data.id];
-  const x = isDragging.value ? draggingPos.value.x : pos.left;
-  const y = isDragging.value ? draggingPos.value.y : pos.top;
+  const x = draggingPos.value ? (draggingPos.value.x - dragOffset.value) : pos.left;
+  const y = draggingPos.value ? draggingPos.value.y : pos.top;
 
   return { x, y, w: pos.width };
 });
 const positionStyles = computed(() => {
   if (!position.value) return '';
 
-  return `--transform: translate(${position.value.x}px, ${position.value.y}px); --width: ${position.value.w}px`;
+  const { w, x, y  } = position.value;
+  return `--transform: translate(${x}px, ${y}px); --width: ${w}px`;
 });
 
 useIntersectionObserver(target, ([{ isIntersecting }]) => {
   targetIsVisible.value = isIntersecting;
 });
 
-useDraggable(target, {
-  onStart: () => {
-    isDragging.value = true;
-    const x = datePositions.value[hoveredDate.value.toFormat('y-MM-dd')];
-    const y = resourcePositions.value[hoveredResourceId.value];
-    draggingPos.value.x = x;
-    draggingPos.value.y = y;
-  },
-  onEnd: () => {
-    isDragging.value = false;
-  },
-  onMove: () => {
-    const x = datePositions.value[hoveredDate.value.toFormat('y-MM-dd')];
-    const y = resourcePositions.value[hoveredResourceId.value];
-    draggingPos.value.x = x;
-    draggingPos.value.y = y;
-  }
-});
+function handleStartDrag(e) {
+  const colOffsetPos = parseInt(e.offsetX / columnWidth, 10) * columnWidth;
+  dragOffset.value = colOffsetPos;
+  isDragging.value = true;
+}
+
+function handleStopDrag(e) {
+  isDragging.value = false;
+}
 </script>
 
 <style scoped>
